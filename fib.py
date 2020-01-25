d71ca7c09e847d7b0a379d20ca55352de70ff7b8
sittthway781 <sittthway781@gmail.com>
2020-01-25 18:02:32 +0630

    first commit

---
+++ fib.py
@@ -0,0 +1,26 @@
+#import fibonacci #import ka akone lone ko kuu
+
+# f = fibonacci.fib
+# f(100)
+
+# fibonacci.fib(100)
+
+# from fibonacci import fib2 #from ka ko lo chin tk fun ko pl khwal pee yu lo ya
+
+
+# f = fib2(500)
+# print (f)
+
+# from fibonacci import *
+# fib(500)
+# print(fib2(200))
+
+# import fibonacci as x # can use the word you want as y or as z 
+
+# x.fib(500)
+
+from fibonacci import fib as fibonacci
+fibonacci(500)
+
+#https://docs.python.org/3/tutorial/modules.html#
+#>>> Student (Import Folder)
